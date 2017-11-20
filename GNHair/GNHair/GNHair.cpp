0bd8ad41500a88db64439596a97a5c7345c1c296
nagasawa <iwbabw.gn@outlook.jp>
2017-11-20 09:57:40 +0900

    comment

--- GNHair/GNHair/GNHair.cpp
+++ GNHair/GNHair/GNHair.cpp
@@ -22,123 +22,131 @@ unsigned int LutgenThreadFunc(void* data)
 	return 0;
 }
 
+
+// パラメタ定義（型・名前・初期値）
 node_parameters
 {
 	AiParameterRGB("dyeColor", 1.0f, 1.0f, 1.0f);
-AiParameterFlt("melanin", 0.15f);
-AiParameterFlt("specularShift", 4.0f);
-AiParameterFlt("specularWidth", 3.0f);
-AiParameterInt("extraSamplesDiffuse", 0);
-AiParameterInt("extraSamplesGlossy", 0);
-AiParameterFlt("diffuseStrength", 1.0f);
-AiParameterRGB("diffuseColor", 1.0f, 1.0f, 1.0f);
-AiParameterEnum("diffuseScatteringMode", DUAL, ScatteringModeNames);
-AiParameterFlt("specular1Strength", 1.0f);
-AiParameterRGB("specular1Color", 1.0f, 1.0f, 1.0f);
-AiParameterFlt("specular2Strength", 1.0f);
-AiParameterRGB("specular2Color",1.0f, 1.0f, 1.0f);
-AiParameterFlt("glintStrength", 2.5f);
-AiParameterFlt("glintRolloff", 5.0f);
-AiParameterFlt("transmissionStrength", 1.0f);
-AiParameterRGB("transmissionColor", 1.0f, 1.0f, 1.0f);
-AiParameterFlt("transmissionRolloff", 10.0f);
-AiParameterRGB("opacity", 1.0f, 1.0f, 1.0f);
-AiParameterInt("dualDepth", 0);
-AiParameterFlt("diffuseForward", 0.7f);
-AiParameterFlt("diffuseBack", 0.7f);
-AiParameterFlt("specular1WidthScale", 1.0f);
-AiParameterFlt("specular2WidthScale", 1.0f);
-AiParameterFlt("transmissionWidthScale", 1.0f);
-AiParameterFlt("specular1Shift", 0.0f);
-AiParameterFlt("specular2Shift", 0.0f);
-AiParameterFlt("transmissionShift", 0.0f);
-AiParameterFlt("diffuseIndirectStrength", 0.0f);
-AiParameterFlt("glossyIndirectStrength", 0.0f);
+	AiParameterFlt("melanin", 0.15f);
+	AiParameterFlt("specularShift", 4.0f);
+	AiParameterFlt("specularWidth", 3.0f);
+	AiParameterInt("extraSamplesDiffuse", 0);
+	AiParameterInt("extraSamplesGlossy", 0);
+	AiParameterFlt("diffuseStrength", 1.0f);
+	AiParameterRGB("diffuseColor", 1.0f, 1.0f, 1.0f);
+	AiParameterEnum("diffuseScatteringMode", DUAL, ScatteringModeNames);
+	AiParameterFlt("specular1Strength", 1.0f);
+	AiParameterRGB("specular1Color", 1.0f, 1.0f, 1.0f);
+	AiParameterFlt("specular2Strength", 1.0f);
+	AiParameterRGB("specular2Color",1.0f, 1.0f, 1.0f);
+	AiParameterFlt("glintStrength", 2.5f);
+	AiParameterFlt("glintRolloff", 5.0f);
+	AiParameterFlt("transmissionStrength", 1.0f);
+	AiParameterRGB("transmissionColor", 1.0f, 1.0f, 1.0f);
+	AiParameterFlt("transmissionRolloff", 10.0f);
+	AiParameterRGB("opacity", 1.0f, 1.0f, 1.0f);
+	AiParameterInt("dualDepth", 0);
+	AiParameterFlt("diffuseForward", 0.7f);
+	AiParameterFlt("diffuseBack", 0.7f);
+	AiParameterFlt("specular1WidthScale", 1.0f);
+	AiParameterFlt("specular2WidthScale", 1.0f);
+	AiParameterFlt("transmissionWidthScale", 1.0f);
+	AiParameterFlt("specular1Shift", 0.0f);
+	AiParameterFlt("specular2Shift", 0.0f);
+	AiParameterFlt("transmissionShift", 0.0f);
+	AiParameterFlt("diffuseIndirectStrength", 0.0f);
+	AiParameterFlt("glossyIndirectStrength", 0.0f);
 
-AiParameterFlt("randomTangent", 0.0f);
-AiParameterFlt("randomHue", 0.0f);
-AiParameterFlt("randomSaturation", 0.0f);
-AiParameterFlt("randomMelanin", 0.08f);
+	AiParameterFlt("randomTangent", 0.0f);
+	AiParameterFlt("randomHue", 0.0f);
+	AiParameterFlt("randomSaturation", 0.0f);
+	AiParameterFlt("randomMelanin", 0.08f);
 
-AiParameterStr("uparam", "uparamcoord");
-AiParameterStr("vparam", "vparamcoord");
+	AiParameterStr("uparam", "uparamcoord");
+	AiParameterStr("vparam", "vparamcoord");
 
-AiParameterRGB("id1", 0.0f, 0.0f, 0.0f);
-AiParameterRGB("id2", 0.0f, 0.0f, 0.0f);
-AiParameterRGB("id3", 0.0f, 0.0f, 0.0f);
-AiParameterRGB("id4", 0.0f, 0.0f, 0.0f);
-AiParameterRGB("id5", 0.0f, 0.0f, 0.0f);
-AiParameterRGB("id6", 0.0f, 0.0f, 0.0f);
-AiParameterRGB("id7", 0.0f, 0.0f, 0.0f);
-AiParameterRGB("id8", 0.0f, 0.0f, 0.0f);
+	AiParameterRGB("id1", 0.0f, 0.0f, 0.0f);
+	AiParameterRGB("id2", 0.0f, 0.0f, 0.0f);
+	AiParameterRGB("id3", 0.0f, 0.0f, 0.0f);
+	AiParameterRGB("id4", 0.0f, 0.0f, 0.0f);
+	AiParameterRGB("id5", 0.0f, 0.0f, 0.0f);
+	AiParameterRGB("id6", 0.0f, 0.0f, 0.0f);
+	AiParameterRGB("id7", 0.0f, 0.0f, 0.0f);
+	AiParameterRGB("id8", 0.0f, 0.0f, 0.0f);
 
-AiParameterStr("aov_diffuse_color", "diffuse_color");
-AiParameterStr("aov_direct_diffuse", "direct_diffuse");
-AiParameterStr("aov_indirect_diffuse", "indirect_diffuse");
-AiParameterStr("aov_direct_local", "direct_local");
-AiParameterStr("aov_indirect_local", "indirect_local");
-AiParameterStr("aov_direct_global", "direct_global");
-AiParameterStr("aov_indirect_global", "indirect_global");
-AiParameterStr("aov_direct_specular", "direct_specular");
-AiParameterStr("aov_indirect_specular", "indirect_specular");
-AiParameterStr("aov_direct_specular_2", "direct_specular_2");
-AiParameterStr("aov_indirect_specular_2", "indirect_specular_2");
-AiParameterStr("aov_direct_glint", "direct_glint");
-AiParameterStr("aov_indirect_glint", "indirect_glint");
-AiParameterStr("aov_direct_transmission", "direct_transmission");
-AiParameterStr("aov_indirect_transmission", "indirect_transmission");
-AiParameterStr("aov_depth", "depth");
-AiParameterStr("aov_light_group_1", "light_group_1");
-AiParameterStr("aov_light_group_2", "light_group_2");
-AiParameterStr("aov_light_group_3", "light_group_3");
-AiParameterStr("aov_light_group_4", "light_group_4");
-AiParameterStr("aov_light_group_5", "light_group_5");
-AiParameterStr("aov_light_group_6", "light_group_6");
-AiParameterStr("aov_light_group_7", "light_group_7");
-AiParameterStr("aov_light_group_8", "light_group_8");
-AiParameterStr("aov_light_group_9", "light_group_9");
-AiParameterStr("aov_light_group_10", "light_group_10");
-AiParameterStr("aov_light_group_11", "light_group_11");
-AiParameterStr("aov_light_group_12", "light_group_12");
-AiParameterStr("aov_light_group_13", "light_group_13");
-AiParameterStr("aov_light_group_14", "light_group_14");
-AiParameterStr("aov_light_group_15", "light_group_15");
-AiParameterStr("aov_light_group_16", "light_group_16");
-AiParameterStr("aov_id_1", "id_1");
-AiParameterStr("aov_id_2", "id_2");
-AiParameterStr("aov_id_3", "id_3");
-AiParameterStr("aov_id_4", "id_4");
-AiParameterStr("aov_id_5", "id_5");
-AiParameterStr("aov_id_6", "id_6");
-AiParameterStr("aov_id_7", "id_7");
-AiParameterStr("aov_id_8", "id_8");
-AiParameterStr("crypto_asset_override", "");
-AiParameterStr("crypto_object_override", "");
-AiParameterStr("crypto_material_override", "");
-AiParameterStr("aov_crypto_asset", "crypto_asset");
-AiParameterStr("aov_crypto_object", "crypto_object");
-AiParameterStr("aov_crypto_material", "crypto_material");
+	AiParameterStr("aov_diffuse_color", "diffuse_color");
+	AiParameterStr("aov_direct_diffuse", "direct_diffuse");
+	AiParameterStr("aov_indirect_diffuse", "indirect_diffuse");
+	AiParameterStr("aov_direct_local", "direct_local");
+	AiParameterStr("aov_indirect_local", "indirect_local");
+	AiParameterStr("aov_direct_global", "direct_global");
+	AiParameterStr("aov_indirect_global", "indirect_global");
+	AiParameterStr("aov_direct_specular", "direct_specular");
+	AiParameterStr("aov_indirect_specular", "indirect_specular");
+	AiParameterStr("aov_direct_specular_2", "direct_specular_2");
+	AiParameterStr("aov_indirect_specular_2", "indirect_specular_2");
+	AiParameterStr("aov_direct_glint", "direct_glint");
+	AiParameterStr("aov_indirect_glint", "indirect_glint");
+	AiParameterStr("aov_direct_transmission", "direct_transmission");
+	AiParameterStr("aov_indirect_transmission", "indirect_transmission");
+	AiParameterStr("aov_depth", "depth");
+	AiParameterStr("aov_light_group_1", "light_group_1");
+	AiParameterStr("aov_light_group_2", "light_group_2");
+	AiParameterStr("aov_light_group_3", "light_group_3");
+	AiParameterStr("aov_light_group_4", "light_group_4");
+	AiParameterStr("aov_light_group_5", "light_group_5");
+	AiParameterStr("aov_light_group_6", "light_group_6");
+	AiParameterStr("aov_light_group_7", "light_group_7");
+	AiParameterStr("aov_light_group_8", "light_group_8");
+	AiParameterStr("aov_light_group_9", "light_group_9");
+	AiParameterStr("aov_light_group_10", "light_group_10");
+	AiParameterStr("aov_light_group_11", "light_group_11");
+	AiParameterStr("aov_light_group_12", "light_group_12");
+	AiParameterStr("aov_light_group_13", "light_group_13");
+	AiParameterStr("aov_light_group_14", "light_group_14");
+	AiParameterStr("aov_light_group_15", "light_group_15");
+	AiParameterStr("aov_light_group_16", "light_group_16");
+	AiParameterStr("aov_id_1", "id_1");
+	AiParameterStr("aov_id_2", "id_2");
+	AiParameterStr("aov_id_3", "id_3");
+	AiParameterStr("aov_id_4", "id_4");
+	AiParameterStr("aov_id_5", "id_5");
+	AiParameterStr("aov_id_6", "id_6");
+	AiParameterStr("aov_id_7", "id_7");
+	AiParameterStr("aov_id_8", "id_8");
+	AiParameterStr("crypto_asset_override", "");
+	AiParameterStr("crypto_object_override", "");
+	AiParameterStr("crypto_material_override", "");
+	AiParameterStr("aov_crypto_asset", "crypto_asset");
+	AiParameterStr("aov_crypto_object", "crypto_object");
+	AiParameterStr("aov_crypto_material", "crypto_material");
 }
 
 
+// ノード読み込み時処理
+// ロードするために必要な情報をセット
 node_loader
 {
 	if (i>0) return 0;
-node->methods = GNHair;
-node->output_type = AI_TYPE_RGB;
-node->name = "GNHair";
-node->node_type = AI_NODE_SHADER;
-strcpy(node->version, AI_VERSION);
-return true;
+	node->methods = GNHair;
+	node->output_type = AI_TYPE_RGB;
+	node->name = "GNHair";
+	node->node_type = AI_NODE_SHADER;
+	strcpy(node->version, AI_VERSION);
+	return true;
 }
 
+
+// ノード初期化時処理
 node_initialize
 {
 	HairBsdf::ShaderData* data = new HairBsdf::ShaderData;
-data->cryptomatte = new CryptomatteData();
-AiNodeSetLocalData(node, data);
+	data->cryptomatte = new CryptomatteData();
+	AiNodeSetLocalData(node, data);
 }
 
+
+// ノード終了（削除）時処理
 node_finish
 {
 	if (AiNodeGetLocalData(node))
@@ -150,113 +158,117 @@ node_finish
 	}
 }
 
+
+// ノード更新時処理
 node_update
 {
 	HairBsdf::ShaderData* data = (HairBsdf::ShaderData*)AiNodeGetLocalData(node);
-data->update(params);
+	data->update(params);
 
-AiAOVRegister("diffuse_color", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
-AiAOVRegister("direct_specular", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
-AiAOVRegister("indirect_specular", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
-AiAOVRegister("direct_specular_2", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
-AiAOVRegister("indirect_specular_2", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
-AiAOVRegister("direct_transmission", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
-AiAOVRegister("indirect_transmission", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
-AiAOVRegister("direct_glint", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
-AiAOVRegister("direct_global", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
-AiAOVRegister("indirect_global", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
-AiAOVRegister("direct_local", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
-AiAOVRegister("indirect_local", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
-AiAOVRegister("id_1", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
-AiAOVRegister("id_2", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
-AiAOVRegister("id_3", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
-AiAOVRegister("id_4", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
-AiAOVRegister("id_5", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
-AiAOVRegister("id_6", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
-AiAOVRegister("id_7", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
-AiAOVRegister("id_8", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
-AiAOVRegister("light_group_1", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
-AiAOVRegister("light_group_2", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
-AiAOVRegister("light_group_3", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
-AiAOVRegister("light_group_4", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
-AiAOVRegister("light_group_5", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
-AiAOVRegister("light_group_6", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
-AiAOVRegister("light_group_7", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
-AiAOVRegister("light_group_8", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
-AiAOVRegister("light_group_9", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
-AiAOVRegister("light_group_10", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
-AiAOVRegister("light_group_11", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
-AiAOVRegister("light_group_12", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
-AiAOVRegister("light_group_13", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
-AiAOVRegister("light_group_14", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
-AiAOVRegister("light_group_15", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
-AiAOVRegister("light_group_16", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
+	AiAOVRegister("diffuse_color", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
+	AiAOVRegister("direct_specular", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
+	AiAOVRegister("indirect_specular", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
+	AiAOVRegister("direct_specular_2", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
+	AiAOVRegister("indirect_specular_2", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
+	AiAOVRegister("direct_transmission", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
+	AiAOVRegister("indirect_transmission", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
+	AiAOVRegister("direct_glint", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
+	AiAOVRegister("direct_global", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
+	AiAOVRegister("indirect_global", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
+	AiAOVRegister("direct_local", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
+	AiAOVRegister("indirect_local", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
+	AiAOVRegister("id_1", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
+	AiAOVRegister("id_2", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
+	AiAOVRegister("id_3", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
+	AiAOVRegister("id_4", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
+	AiAOVRegister("id_5", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
+	AiAOVRegister("id_6", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
+	AiAOVRegister("id_7", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
+	AiAOVRegister("id_8", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
+	AiAOVRegister("light_group_1", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
+	AiAOVRegister("light_group_2", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
+	AiAOVRegister("light_group_3", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
+	AiAOVRegister("light_group_4", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
+	AiAOVRegister("light_group_5", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
+	AiAOVRegister("light_group_6", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
+	AiAOVRegister("light_group_7", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
+	AiAOVRegister("light_group_8", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
+	AiAOVRegister("light_group_9", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
+	AiAOVRegister("light_group_10", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
+	AiAOVRegister("light_group_11", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
+	AiAOVRegister("light_group_12", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
+	AiAOVRegister("light_group_13", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
+	AiAOVRegister("light_group_14", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
+	AiAOVRegister("light_group_15", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
+	AiAOVRegister("light_group_16", AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
 
-data->cryptomatte->setup_all(AiNodeGetStr(node, "aov_crypto_asset"),
+	data->cryptomatte->setup_all(AiNodeGetStr(node, "aov_crypto_asset"),
 	AiNodeGetStr(node, "aov_crypto_object"), AiNodeGetStr(node, "aov_crypto_material"));
-// Get all the light nodes in the scene and try and find their light group parameter
-// we'll store this based on the light pointer for fast access during rendering
-AtNodeIterator* it = AiUniverseGetNodeIterator(AI_NODE_LIGHT);
-while (!AiNodeIteratorFinished(it))
-{
-	AtNode* light = AiNodeIteratorGetNext(it);
-	if (AiNodeLookUpUserParameter(light, "lightGroup"))
-		data->lightGroups[light] = AiNodeGetInt(light, "lightGroup") - 1;
-	else
-		data->lightGroups[light] = -1;
-}
-}
 
+	// Get all the light nodes in the scene and try and find their light group parameter
+	// we'll store this based on the light pointer for fast access during rendering
+	AtNodeIterator* it = AiUniverseGetNodeIterator(AI_NODE_LIGHT);
+	while (!AiNodeIteratorFinished(it))
+	{
+		AtNode* light = AiNodeIteratorGetNext(it);
+		if (AiNodeLookUpUserParameter(light, "lightGroup"))
+			data->lightGroups[light] = AiNodeGetInt(light, "lightGroup") - 1;
+		else
+			data->lightGroups[light] = -1;
+	}
+}
+// シェーダメイン処理（ピクセル単位）
 shader_evaluate
 {
 
 	// Get shader data
 	HairBsdf::ShaderData* data = (HairBsdf::ShaderData*)AiNodeGetLocalData(node);
 
-// Create HairBsdf object 
-HairBsdf hb(node, sg, data);
-// Get parameters
-hb.evaluateParameters(sg, data);
+	// Create HairBsdf object 
+	HairBsdf hb(node, sg, data);
 
-float geo_opacity = 1.0f;
-if (AiUDataGetFlt("geo_opacity", &geo_opacity))
-{
-	hb.opacity *= geo_opacity;
-}
+	// Get parameters
+	hb.evaluateParameters(sg, data);
 
-const bool transp_early_out = AiShaderGlobalsApplyOpacity(sg, hb.opacity);
-data->cryptomatte->do_cryptomattes(sg, node, p_crypto_asset_override, p_crypto_object_override, p_crypto_material_override);
-if (transp_early_out)
-return;
-
-hb.opacity = sg->out_opacity;
-
-AiStateSetMsgBool("als_hitHair", true);
-
-float als_hairNumIntersections = 0;
-AtRGB als_sigma_bar_f = AI_RGB_BLACK;
-bool do_dual = false;
-if (sg->Rr >= data->dual_depth) do_dual = true;
-
-int als_raytype = ALS_RAY_UNDEFINED;
-AiStateGetMsgInt("als_raytype", &als_raytype);
-
-if (do_dual && als_raytype == ALS_RAY_DUAL)
-{
-	if (AiStateGetMsgFlt("als_hairNumIntersections", &als_hairNumIntersections)
-		&& AiStateGetMsgRGB("als_sigma_bar_f", &als_sigma_bar_f))
+	float geo_opacity = 1.0f;
+	if (AiUDataGetFlt("geo_opacity", &geo_opacity))
 	{
-		float theta_i = AI_PIOVER2 - sphericalTheta(sg->Rd, hb.U);
-
-		als_sigma_bar_f += hb.sp.beta_R2 + hb.sp.beta_TRT2 + hb.sp.beta_TT2;
-		AiStateSetMsgRGB("als_sigma_bar_f", als_sigma_bar_f);
-
-		als_hairNumIntersections += minh(hb.opacity);
-		AiStateSetMsgFlt("als_hairNumIntersections", als_hairNumIntersections);
-
-		sg->out_opacity = AI_RGB_BLACK;
+		hb.opacity *= geo_opacity;
 	}
-	else
+
+	const bool transp_early_out = AiShaderGlobalsApplyOpacity(sg, hb.opacity);
+	data->cryptomatte->do_cryptomattes(sg, node, p_crypto_asset_override, p_crypto_object_override, p_crypto_material_override);
+	if (transp_early_out)
+	return;
+
+	hb.opacity = sg->out_opacity;
+
+	AiStateSetMsgBool("als_hitHair", true);
+
+	float als_hairNumIntersections = 0;
+	AtRGB als_sigma_bar_f = AI_RGB_BLACK;
+	bool do_dual = false;
+	if (sg->Rr >= data->dual_depth) do_dual = true;
+
+	int als_raytype = ALS_RAY_UNDEFINED;
+	AiStateGetMsgInt("als_raytype", &als_raytype);
+
+	if (do_dual && als_raytype == ALS_RAY_DUAL)
+	{
+		if (AiStateGetMsgFlt("als_hairNumIntersections", &als_hairNumIntersections)
+			&& AiStateGetMsgRGB("als_sigma_bar_f", &als_sigma_bar_f)) 
+		{
+			float theta_i = AI_PIOVER2 - sphericalTheta(sg->Rd, hb.U);
+
+			als_sigma_bar_f += hb.sp.beta_R2 + hb.sp.beta_TRT2 + hb.sp.beta_TT2;
+			AiStateSetMsgRGB("als_sigma_bar_f", als_sigma_bar_f);
+
+			als_hairNumIntersections += minh(hb.opacity);
+			AiStateSetMsgFlt("als_hairNumIntersections", als_hairNumIntersections);
+
+			sg->out_opacity = AI_RGB_BLACK;
+	}
+	else 
 	{
 		sg->out_opacity = AI_RGB_WHITE;
 	}
